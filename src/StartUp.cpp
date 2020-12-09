6ba395099d1c2258a4dcd9784578085e84ca2d1a
Jeremy Castagno <jeremybyu@gmail.com>
2020-12-09 15:32:48 -0500

    Update startup program

---
+++ src/StartUp.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+int main()
+{
+
+  std::cout << "Welcome to the C++ Loop Tutorial running on Repl.it!" << std::endl;
+  std::cout << "All programs have already been compiled.  You can run them by typing './cmake-build/bin/Loop1' or './cmake-build/bin/Loop2' in the terminal below." << std::endl;
+  std::cout << "You are also welcome to modify the programs in the editor on the left." << std::endl;
+  std::cout << "To recompile your changes, please hit the green RUN at the top of the screen." << std::endl;
+  std::cout << "This powerpoint can also be downloaded to help follow along: https://github.com/JeremyBYU/LoopIt/blob/master/Lecture/LoopIt.pptx" << std::endl;
+}
