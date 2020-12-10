e0b33ab2ffe06ba831c16bd464b031c3f4da43e2
Jeremy Castagno <jeremybyu@gmail.com>
2020-12-10 16:23:46 -0500

    Update For4

--- src/For4.cpp
+++ src/For4.cpp
@@ -3,15 +3,16 @@
 
 int main()
 {
-  const int rows = 8;
-  const int cols = 8;
+  const int rows = 5;
+  const int cols = 5;
   // Outer For Loop
   for (int i = 0; i < rows; ++i)
   { 
     // Inner for loop
     for (int j = 0; j < cols; ++j)
     {
-      std::cout << "#" << " ";
+      // print out (i,j)
+      std::cout << "(" << i << "," << j << ")" << " ";
     }
     std::cout << std::endl;
   }
